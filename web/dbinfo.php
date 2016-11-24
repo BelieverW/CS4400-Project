2ed2a12013c20067e4a9be674a5dfac4b52ffa35
BelieverW <hlwang0309@hotmail.com>
2016-11-24 09:41:04 -0500

    modified

---
+++ web/dbinfo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Huanli
+ * Date: 24/11/2016
+ * Time: 09:39
+ */
+$username = 'xxxxxxxxxxxxxx';
+$password = 'xxxxxxxxxxx';
+$host = 'localhost';
+$database = 'cs4400_example';
+
+?>
