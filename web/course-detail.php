4c17768d5b1729639a8ffbb4c3bdaf1d5418b9ed
BelieverW <hlwang0309@hotmail.com>
2016-12-01 21:11:08 -0500

    test

--- web/course-detail.php
+++ web/course-detail.php
@@ -51,12 +51,6 @@ if(!isset($_REQUEST['coursenumber'])) {
 
 
     <style type="text/css">
-        #line-chart {
-            height:300px;
-            width:800px;
-            margin: 0px auto;
-            margin-top: 1em;
-        }
         .navbar-default .navbar-brand, .navbar-default .navbar-brand:hover { 
             color: #fff;
         }
